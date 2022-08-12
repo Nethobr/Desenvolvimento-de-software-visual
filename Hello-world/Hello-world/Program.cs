d8a96465cdbe6d55bb9ff0f98cae0a3c36572cf4
aluno <Aluno@DEVE009812.educacional.up>
2022-08-11 21:50:58 -0300

    11082022

---
+++ Hello-world/Hello-world/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        // Função Main
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            Console.Write("Largura: ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Resultado: " + CalculaArea(largura, altura) + ".");
+        }   // Fim Main
+
+        // Função CalculaArea
+        static double CalculaArea(double largura, double altura)
+        {
+            return largura * altura;
+        }   // Fim CalculaArea
+    }   // Fim Classe Program
+}   // Fim Hello_world
