1edd267a14a4c9213a4d374df8a03c592a193da9
aluno <Aluno@DEVE009812.educacional.up>
2022-08-18 20:01:30 -0300

    18082022

---
+++ Repetidores/Repetidores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Repetidores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva o valor para o Fibonacci: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            int ultimo = 1, penultimo = 0;
+
+            for (int i = 0; i < valor; i++)
+            {
+                int proximo = ultimo + penultimo;
+                Console.WriteLine(proximo);
+                penultimo = ultimo;
+                ultimo = proximo;
+            }   // Fim for
+        }   // Fim Main
+    }   // Fim Program
+}   // Fim Repetidores
