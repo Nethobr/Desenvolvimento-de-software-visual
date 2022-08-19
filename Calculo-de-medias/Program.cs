7a35efa46926dee3556e53d7f898b676c2bd9586
aluno <Aluno@DEVE009812.educacional.up>
2022-08-18 22:07:38 -0300

    18082022

--- Calculo-de-medias/Program.cs
+++ Calculo-de-medias/Program.cs
@@ -6,20 +6,56 @@ namespace Calculo_de_medias
     {
         static void Main(string[] args)
         {
-            double [] media = {1, 5, 9, 15, 30};
-            Console.WriteLine(calculaMedia(media));
+            double [] media = {10, 15, 15, 20, 20, 20, 25};
+            Console.WriteLine("Média: " + calculaMedia(media) + ".");
+            Console.WriteLine("Mediana: "+ calculaMediana(media) + ".");
+            Console.WriteLine("Moda: "+ calculoModa(media) + ".");
         }   // Fim Main
 
         static double calculaMedia (double [] media)
         {   
             double total = 0;
-            int count = 0;
             for (int i = 0; i < media.Length; i++)
             {
                 total += media[i];
-                count = i+1;
             }   // Fim for
-            return total / count;
+            return total / media.Length;
         }   // Fim Media
+        
+        static double calculaMediana (double [] media)
+        {   
+            int meio = media.Length / 2;
+            Array.Sort(media);
+            if ((media.Length % 2) == 0)
+                return (media[meio] + media[meio - 1]) / 2 ;
+            else
+                return media[meio];
+        }   // Fim calculaMediana
+
+        static double calculoModa (double [] media)
+        {
+            double [] outro = new double [media.Length];
+
+            for (int i = 0; i < media.Length; i++)
+            {
+                int count = 0;
+                double num = media[i];
+                for (int j = 0; j < media.Length; j++)
+                {
+                    if (media[j] == num)
+                        count ++;
+                }   // Fim for
+                outro[i] = count;
+            }   // Fir for
+
+            int max = 0;
+            for (int i = 0; i < outro.Length; i++)
+            {
+                if (outro[i] >= max)
+                    max = i;
+            }   // Fim for
+
+            return media[max];
+        }   // Fim calculoModa
     }   // Fim Program
 }   // Fim Calculo_de_medias
