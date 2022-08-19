c60f146592809ad6ed76004a70b66e6faf21c853
aluno <Aluno@DEVE009812.educacional.up>
2022-08-18 21:00:33 -0300

    18082022

---
+++ Calculo-de-medias/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculo_de_medias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] media = {1, 5, 9, 15, 30};
+            Console.WriteLine(calculaMedia(media));
+        }   // Fim Main
+
+        static double calculaMedia (double [] media)
+        {   
+            double total = 0;
+            int count = 0;
+            for (int i = 0; i < media.Length; i++)
+            {
+                total += media[i];
+                count = i+1;
+            }   // Fim for
+            return total / count;
+        }   // Fim Media
+    }   // Fim Program
+}   // Fim Calculo_de_medias
